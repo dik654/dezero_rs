0b4716b4efb3c3c6864631cf26134d8fbd1d991e
김대익 <33992354+dik654@users.noreply.github.com>
2026-02-13 09:41:15 +0900

    feat: add test_insert_new_data

--- steps/step01.rs
+++ steps/step01.rs
@@ -1,16 +1,16 @@
+use ndarray::ArrayD;
+
 struct Variable {
-    data: f64,
+    data: ArrayD<f64>,
 }
 
+// 초기화 함수
 impl Variable {
-    fn new(data: f64) -> Self {
+    // 인수를 인스턴스 변수에 저장
+    // 머신 러닝 시스템은 기본 데이터 구조로 다차원 배열 사용
+    fn new(data: ArrayD<f64>) -> Self {
         Variable { data }
     }
-}
-
-fn main() {
-    let x = Variable::new(1.0);
-    println!("x.data = {}", x.data);
 }
 
 #[cfg(test)]
@@ -18,8 +18,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_variable() {
-        let x = Variable::new(1.0);
-        assert_eq!(x.data, 1.0);
+    fn test_init_variable() {
+        // 0차원 스칼라값 1을 인스턴스에 넣기
+        let x = Variable::new(ndarray::arr0(1.0).into_dyn());
+        // 데이터가 제대로 담겨있는지 확인
+        assert_eq!(x.data, ndarray::arr0(1.0).into_dyn());
+    }
+
+    #[test]
+    fn test_insert_new_data() {
+        // 0차원 스칼라값 1을 인스턴스에 넣기
+        // 인스턴스의 변수가 바뀔 것이므로 가변(mut) 선언
+        let mut x = Variable::new(ndarray::arr0(1.0).into_dyn());
+        // 인스턴스에 새로운 데이터 대입
+        x.data = ndarray::arr0(2.0).into_dyn();
+        // 새로운 데이터가 대입되었는지 확인
+        assert_eq!(x.data, ndarray::arr0(2.0).into_dyn());
     }
 }
